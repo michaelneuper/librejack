80a730f1cca8f5ceac80642d5f17b87bcc343a8e
Michael Neuper <73108749+michaelneuper@users.noreply.github.com>
2022-05-08 17:28:29 +0200

    update Player.java

--- src/Model/Player.java
+++ src/Model/Player.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
 
 /**
@@ -10,5 +5,6 @@ package Model;
  * @author michael
  */
 public class Player {
+    // TODO
     
 }
