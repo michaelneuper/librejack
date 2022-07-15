44505942c5ad4b8126aa6542cb5a479cb32a0334
Michael Neuper <73108749+michaelneuper@users.noreply.github.com>
2022-07-15 23:54:40 +0200

    create Controller.java

---
+++ src/controller/Controller.java
@@ -0,0 +1,20 @@
+package controller;
+
+/**
+ * Handles most of the game logic
+ * @author michael
+ */
+public class Controller {
+    
+    private int wins, losses, pushes;
+
+    /**
+     * Controller constructor
+     * Initialises score to 0
+     */
+    public Controller() {
+        wins = 0;
+        losses = 0;
+        pushes = 0;
+    }
+}
