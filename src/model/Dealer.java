5550c16ba822a44c547fd8769f4f0e3181e5f328
Michael Neuper <73108749+michaelneuper@users.noreply.github.com>
2022-07-15 23:55:05 +0200

    create Dealer.java

---
+++ src/model/Dealer.java
@@ -0,0 +1,10 @@
+package model;
+
+/**
+ * Handles dealer specific operations, such as drawing cards to 16 and standing on 17 or above.
+ * @author michael
+ */
+public class Dealer extends Person {
+    
+    Deck dealerDeck = new Deck();
+}
