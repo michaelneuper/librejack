e129e1e358183bd0b16d0073d034ff38569a387b
Michael Neuper <73108749+michaelneuper@users.noreply.github.com>
2022-10-07 18:50:46 +0200

    Update Main.java

--- src/view/Main.java
+++ src/view/Main.java
@@ -2,9 +2,13 @@ package view;
 
 import controller.Controller;
 import model.Card;
+import model.Deck;
 import model.Card.Rank;
 import model.Card.Suit;
 
+/**
+ * @author Michael
+ */
 public class Main {
     
     public static void main(String[] args) {
@@ -13,9 +17,17 @@ public class Main {
         
         Controller blackjack = new Controller();
 
-        Card testCard = new Card(Suit.CLUBS, Rank.ACE);
-
-        System.out.println(testCard);
-    }
+        //Make some cards and a deck
+        Deck testDeck = new Deck();
+        Card aCard = new Card(Suit.CLUBS, Rank.QUEEN);
+        Card bCard = new Card(Suit.DIAMONDS,Rank.ACE);
+        Card cCard = new Card(Suit.SPADES, Rank.SIX);
+        //Add the cards to the deck
+        testDeck.addCard(aCard);
+        testDeck.addCard(bCard);
+        testDeck.addCard(cCard);
+        //Print out the deck
+        System.out.println(testDeck);
+        }
     
 }
