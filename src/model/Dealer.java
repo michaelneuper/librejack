bf602d070fc865a0d3fe73ffe06e1a8de88ac7c6
Michael Neuper <73108749+michaelneuper@users.noreply.github.com>
2022-10-07 21:05:09 +0200

    update Dealer.java

--- src/model/Dealer.java
+++ src/model/Dealer.java
@@ -1,10 +1,22 @@
 package model;
 
 /**
- * Handles dealer specific operations, such as drawing cards to 16 and standing on 17 or above.
- * @author michael
+ * Handles dealer specific operations,
+ * such as drawing cards to 16 and standing
+ * on 17 or above.
+ * 
+ * @author Michael
  */
 public class Dealer extends Person {
-    
-    Deck dealerDeck = new Deck();
+
+    /**
+     * Creates a new dealer
+     */
+    public Dealer() {
+        super.setName("Dealer");
+    }
+
+    public void printFirstHand() {
+        System.out.println("Dealer's hand:\n" + super.getHand().getCard(0));
+    }
 }
